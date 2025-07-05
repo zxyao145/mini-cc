c698ec481f1ff1cf10103fc24fdf44c18f75d39c
zxyao145 <zxyao145@gmail.com>
2025-07-05 21:35:26 +0800

    refacto: add utils.IsApiRequest

---
+++ src/backend/MiniCc.Api/Common/Utils.cs
@@ -0,0 +1,12 @@
+﻿namespace MiniCc.Api.Common;
+
+public class Utils
+{
+    // 帮助方法：检查是否为API请求
+    public static bool IsApiRequest(HttpRequest request)
+    {
+        return request.Path.StartsWithSegments("/api") ||
+               request.Headers["Accept"].ToString().Contains("application/json") ||
+               request.Headers["Content-Type"].ToString().Contains("application/json");
+    }
+}
