5f5744cfcb99bdfb1fcac927e2625618c1fdc842
zxyao <zxyao145@gmail.com>
2025-07-07 18:57:18 +0800

    fix AccessKeyService.cs

--- src/backend/MiniCc.Api/Services/AccessKeyService.cs
+++ src/backend/MiniCc.Api/Services/AccessKeyService.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore;
 using MiniCc.Api.Data;
 using MiniCc.Api.Models;
 
@@ -17,23 +16,25 @@ public class AccessKeyService : IAccessKeyService
 {
     private readonly MiniCcContext _context;
     private readonly ILogger<AccountService> _logger;
-    private readonly IEncryptionService _encryptionService;
 
-    public AccessKeyService(MiniCcContext context, ILogger<AccountService> logger, IEncryptionService encryptionService)
+    public AccessKeyService(MiniCcContext context, ILogger<AccountService> logger)
     {
         _context = context;
         _logger = logger;
-        _encryptionService = encryptionService;
     }
 
-    public Task<bool> IsValid(string key)
+    public async Task<bool> IsValid(string key)
     {
         if (string.IsNullOrWhiteSpace(key))
         {
-            return Task.FromResult(false);
+            return false;
         }
-        var encryptKey = _encryptionService.Encrypt(key);
-        return _context.AccessKeys.AnyAsync(x => x.Key == encryptKey);
+        var query = _context.AccessKeys.Where(x => x.Key == key);
+        _logger.LogDebug("Checking if access key is valid: {query}", query.ToQueryString());
+        return await  _context.AccessKeys.AnyAsync(x=>x.Key == key
+         && !x.Disabled
+         && (x.ExpiredTime == null || x.ExpiredTime > DateTimeOffset.UtcNow)
+         );
     }
 
     public Task<List<AccessKey>> List(Guid userId)
